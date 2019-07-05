dc6ebab423dd0266986ffe226aa8b1cc77bd2ae6
Shawn Bonetti <smb1037@sru.edu>
2019-07-04 17:35:11 -0700

    First commit

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    hostName = StringField('Enter the host name (DNS) of the server to connect to:', validators=[DataRequired()])
+    portNumber = IntegerField('Please enter the port number:', validators=[DataRequired()])
+    userName = StringField('Enter your irods user name:', validators=[DataRequired()])
+    irodsZone = StringField('Enter your irods zone:', validators=[DataRequired()])
+    password = PasswordField('Enter your current irods password:', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+class NewCollectionForm(FlaskForm):
+   collectionName = StringField('Enter collection name:', validators=[DataRequired()])
+   attributesAndValues = StringField('Attributes and values:', validators=[DataRequired()])
+   submit = SubmitField('Create new collection')
+
+class ModifyCollectionForm(FlaskForm):
+   attributesAndValues = StringField('Attributes and values:', validators=[DataRequired()])
+   submit = SubmitField('Modify Collection')
+
+class SearchForm(FlaskForm):
+   collect = SelectField(u'Collections' , choices = [('number1', 'NUMBER1'), ('number2', 'NUMBER2')])
+   submit = SubmitField('Submit')
---
+++ app/routes.py
@@ -0,0 +1,43 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm, NewCollectionForm, ModifyCollectionForm, SearchForm
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/newCollection')
+def newCollection():
+   form = NewCollectionForm()
+   return render_template('newCollection.html', title='New Collection', form=form)
+
+@app.route('/modifyCollection')
+def modifyCollection():
+   form = ModifyCollectionForm()
+   return render_template('modifyCollection.html', title='Modify Collection', form=form)
+
+@app.route('/search')
+def search():
+   form = SearchForm()
+   return render_template('search.html', title='Search', form=form)
